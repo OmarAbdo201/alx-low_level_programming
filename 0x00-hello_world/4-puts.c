c581a7b5d3cf19e5a56d3eec0aed2961164fe899
OmarAbdo201 <omar30abdo30@gmail.com>
2023-03-20 08:50:26 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /*
- * main .A program print a line that prints a line with puts function
+ * int main print a statment program print a line that prints a line with puts function
  * return: 0 (finish)
 */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
