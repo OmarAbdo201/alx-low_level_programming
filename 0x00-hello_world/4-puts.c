872871b381f12e7b9f7a3011c3939b9cb42cb845
OmarAbdo201 <omar30abdo30@gmail.com>
2023-03-20 07:25:11 -0700

    my com

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main . puts : "Programming is like building a multilingual puzzle
- * return : 0 @ finish
+ * main .A program print a line that prints a line with puts function
+ * return; 0 (finish)
 */
 
 int main(void)
