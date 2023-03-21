06caf07fbf64e7f3c8bd35255c0b8088837c3db3
OmarAbdo201 <omar30abdo30@gmail.com>
2023-03-21 08:35:47 -0700

    my commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,16 +5,15 @@
 	 * print_alphabet - print all alphabet in lowercase
 	 */
 
-void print_alphabet(void);
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-print_alphabet(void){
-	char letter;
-	for(letter = 'a', letter <= 'z',letter ++)
-		_putchar(letter);
+
+	void print_alphabet(void)
+	{
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
 		_putchar('\n');
-		return (0);
-}
+	}
