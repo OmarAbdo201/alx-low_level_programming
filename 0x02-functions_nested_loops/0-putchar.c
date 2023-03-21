683c7fa7e6a432ee8d619d89ff801e1c9957c8aa
OmarAbdo201 <omar30abdo30@gmail.com>
2023-03-21 08:32:47 -0700

    my commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,16 @@
 	 * Return: On success 1.
 	 * On error, -1 is returned, and errno is set appropriately.
 	 */
-	int _putchar(char c)
-	{
-		return (write(1, &c, 1));
-	}
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+        putchar('c');
+        putchar('h');
+        putchar('a');
+        putchar('r');
+        putchar('\n');
+	return (0);
+}
