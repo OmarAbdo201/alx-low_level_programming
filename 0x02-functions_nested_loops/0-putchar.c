51a2338c2c7ab4e6c1184d34a9350ccf80a0a902
OmarAbdo201 <omar30abdo30@gmail.com>
2023-03-21 06:10:47 -0700

    my first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : prints _putchar in main fun
+ *
+ * Reaturn : Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
